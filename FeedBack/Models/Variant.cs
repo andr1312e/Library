1fa51d881f91cf6b2a09c6f7de589cf42789ea9b
andr1312e <andr1312e@gmail.com>
2020-01-15 10:59:42 +0300

    Добавьте файлы проекта.

---
+++ FeedBack/Models/Variant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace FeedBack.Models
+{
+    public class Variant
+    {
+        public List<object> questModels;
+
+        public Variant() 
+        {
+            questModels = new List<object>();
+        }
+    }
+}
